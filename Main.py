1520008962f89693a12dc834d3ffcca896c06639
jakob676757 <jakob.kaspar@icloud.com>
2026-06-30 22:56:23 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,12 @@
+import discord
+import os
+
+TOKEN = os.getenv('TOKEN')
+
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f'Bot {client.user} ist online!')
+
+client.run(TOKEN)
